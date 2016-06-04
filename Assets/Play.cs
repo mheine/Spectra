cb0126710b10866675391cf7dd1c900ff9533943
ludwan <ludwigsidenmark@gmail.com>
2016-06-04 16:39:56 +0200

    Some additions

---
+++ Assets/Play.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<AudioSource>().Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
