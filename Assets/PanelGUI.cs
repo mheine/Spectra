1cda108c1f99f03121e40c2c39a447fd0ae2a493
Marcus Heine <mheine@kth.se>
2016-06-20 19:21:33 +0200

    Adds a starting GUI with clickable buttons in a list

---
+++ Assets/PanelGUI.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class PanelGUI : MonoBehaviour {
+
+    private bool isShowing;
+    private string dir;
+
+
+	// Use this for initialization
+	void Start () {
+
+        isShowing = true;
+        int counter = 0;
+        dir = "C:\\Users\\Marcus\\Music\\Pendulum";
+
+
+
+        string[] listOfFiles = Directory.GetFiles(dir);
+        List<string> filesAsList = new List<string>();
+
+
+        var button = GameObject.Find("B1");
+        GameObject grid = GameObject.Find("Grid");
+
+        foreach (string filename in listOfFiles)
+        {
+            if(filename.EndsWith(".mp3"))
+            {
+                filesAsList.Add(filename);
+            }
+        }
+
+
+            foreach (string filename in filesAsList)
+        {
+     
+            var btn = (GameObject)Instantiate(button, transform.position, Quaternion.identity);
+            btn.name = "B_" + counter;
+            btn.transform.parent = grid.transform;
+            btn.GetComponentInChildren<Text>().text = filename.Substring(dir.Length+1, filename.Length-(dir.Length + 5));
+            counter++;
+        }
+        button.SetActive(false);
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        GameObject canvasObject = GameObject.Find("Canvas");
+        GameObject list = GameObject.Find("ListField");
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            isShowing = !isShowing;
+            canvasObject.GetComponent<Canvas>().enabled = isShowing;
+        }
+
+    }
+}
