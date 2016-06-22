e702abb6922a172d6d2ac6079cfcd360e0ef56b6
Marcus Heine <mheine@kth.se>
2016-06-22 17:46:01 +0200

    Trying song switches

--- Assets/PanelGUI.cs
+++ Assets/PanelGUI.cs
@@ -3,34 +3,38 @@ using UnityEngine.UI;
 using System.Collections;
 using System.IO;
 using System.Collections.Generic;
+[RequireComponent (typeof (AudioSource))]
 
 public class PanelGUI : MonoBehaviour {
 
     private bool isShowing;
+    private bool isPlaying;
     private string dir;
     private int counter;
+    public AudioSource sound;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         isShowing = true;
+        isPlaying = true;
         counter = 0;
 
         //Hard-coded directory, for now
-        dir = "C:\\Users\\Marcus\\Music\\Pendulum";
+        dir = Application.dataPath + "//Resources";
 
         string[] listOfFiles = Directory.GetFiles(dir);
         List<string> filesAsList = new List<string>();
 
-
-        Button button = GameObject.Find("B1").GetComponent<Button>();
         GameObject grid = GameObject.Find("Grid");
+        Button button = GameObject.Find("B1").GetComponent<Button>();
+        
 
         //Simple filter for .mp3 files
         foreach (string filename in listOfFiles)
         {
-            if(filename.EndsWith(".mp3"))
+            if(filename.EndsWith(".mp3") || filename.EndsWith(".wav"))
                 filesAsList.Add(filename);
         }
 
@@ -42,7 +46,13 @@ public class PanelGUI : MonoBehaviour {
             var btn = (Button)Instantiate(button, transform.position, Quaternion.identity);
             string strippedFilename = filename.Substring(dir.Length + 1, filename.Length - (dir.Length + 5));
 
+
+            
+
             btn.name = "B_" + strippedFilename;
+            //sound = btn.GetComponent<AudioSource>();
+            //sound.clip = clip;
+            //btn.GetComponent<AudioSource>().clip = clip;
 
             //Set the grid as the parent
             btn.transform.SetParent(grid.transform, false);
@@ -51,7 +61,7 @@ public class PanelGUI : MonoBehaviour {
             btn.GetComponentInChildren<Text>().text = strippedFilename;
 
             //Add an actionlistener to each button
-            btn.onClick.AddListener(delegate { test(btn.name); });
+            btn.onClick.AddListener(delegate { PlayOrPause(strippedFilename); });
 
             counter++;
         }
@@ -63,9 +73,14 @@ public class PanelGUI : MonoBehaviour {
 
     }
 
-    void test(string s)
-    {
-        print("We got a click from button: " + s);
+    void PlayOrPause(string dir)
+    {  
+        var clip = Resources.Load(dir) as AudioClip;
+        sound = GameObject.Find("Audio_Source").GetComponent<AudioSource>();
+
+        sound.Stop();
+        sound.clip = clip;
+        sound.Play();
     }
 
     // Update is called once per frame
@@ -77,6 +92,21 @@ public class PanelGUI : MonoBehaviour {
         {
             isShowing = !isShowing;
             canvasObject.GetComponent<Canvas>().enabled = isShowing;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            var sound = GameObject.Find("Audio_Source").GetComponent<AudioSource>();
+            if (isPlaying) {
+                sound.Pause();
+                isPlaying = !isPlaying;
+            }
+
+            else {
+                sound.Play();
+                isPlaying = !isPlaying;
+            }
+                
         }
 
     }
