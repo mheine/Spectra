4a56532656fcac7dd4fb04a5051c35124652ab08
Marcus Heine <mheine@kth.se>
2017-11-14 18:29:44 +0100

    Adds script to go from start screen to main scene

---
+++ Assets/Scripts/LoadMainScene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class LoadMainScene : MonoBehaviour {
+
+    private bool loadScene = false;
+
+    [SerializeField]
+    private int scene;
+    [SerializeField]
+    private Text loadingText;
+
+    void Start() {
+    	loadingText.text = "Loading, please wait...";
+        StartCoroutine(LoadNewScene());
+        loadingText.color = new Color(loadingText.color.r, loadingText.color.g, loadingText.color.b, Mathf.PingPong(Time.time, 1));
+    }
+
+
+    // Updates once per frame
+    void Update() {
+    }
+
+
+    // The coroutine runs on its own at the same time as Update() and takes an integer indicating which scene to load.
+    IEnumerator LoadNewScene() {
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync("Assets/Scenes/MainScene.unity");
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone) {
+            yield return null;
+        }
+
+    }
+
+}
